50d205a5818486c4fe17021ab1f81da894000fc4
HareemSaad <88980688+HareemSaad@users.noreply.github.com>
2022-09-21 21:52:52 +0500

    Add files via upload

---
+++ Tx.origin Phishing Attack/SafeContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Wallet {
+    address public owner;
+
+    constructor() payable {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable{
+        require(owner==msg.sender);
+    }
+
+    function transfer(address payable _to, uint _amount) public {
+        require(msg.sender == owner, "Not owner");
+
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    function balanceOf(address _address) public view returns(uint256) {
+        return _address.balance;
+    }
+}
